d4fdf4b8ef06cd4d531315d54ba40b8b24dd9de2
phopha <148693028+phopha@users.noreply.github.com>
2025-08-15 13:56:28 +0700

    10. Camera (Perspective Camera)

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,15 @@
 import './style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
-import States from 'three/addons/libs/stats.module.js'
-import {GUI} from 'dat.gui'
+import Stats from 'three/addons/libs/stats.module.js'
+import { GUI } from 'dat.gui'
 
-// const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0x123456)
-// scene.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
-// scene.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
-// scene.backgroundBlurriness = 0.5
+const scene = new THREE.Scene()
+scene.add(new THREE.GridHelper())
 
-const sceneA = new THREE.Scene()
-sceneA.background = new THREE.Color(0x123456)
-
-const sceneB = new THREE.Scene()
-sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
-
-const sceneC = new THREE.Scene()
-sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
+// window.innerWidth / window.innerHeight
+const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 1000)
+camera.position.set(0, 2, 3)
+camera.lookAt(0, 0.5, 0)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -32,64 +21,44 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-new OrbitControls(camera, renderer.domElement)
-
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-// scene.add(cube)
-sceneA.add(cube)
-sceneC.add(cube)
+cube.position.y = 0.5
+scene.add(cube)
 
-const stats = new States();
-stats.showPanel(1);
-document.body.appendChild(stats.dom);
-
-// const gui = new GUI();
-// gui.add(cube.rotation, 'x', 0, Math.PI * 2)
-
-// const cubeFolder = gui.addFolder('Cube')
-// cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-// cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-// cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-// cubeFolder.open()
-
-// const cameraFolder = gui.addFolder('Camera')
-// cameraFolder.add(camera.position, 'x', 0, 20)
-// cameraFolder.add(camera.position, 'y', 0, 20)
-// cameraFolder.add(camera.position, 'z', 0, 20)
-// cameraFolder.open()
-
-let activeScene = sceneA
-const setScene = {
-  sceneA: () => {
-    activeScene = sceneA
-  },
-  sceneB: () => {
-    activeScene = sceneB
-  },
-  sceneC: () => {
-    activeScene = sceneC
-  },
-}
+const stats = new Stats()
+document.body.appendChild(stats.dom)
 
 const gui = new GUI()
-gui.add(setScene, 'sceneA').name('Scene A')
-gui.add(setScene, 'sceneB').name('Scene B')
-gui.add(setScene, 'sceneC').name('Scene C')
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'x', -10, 10)
+cameraFolder.add(camera.position, 'y', -10, 10)
+cameraFolder.add(camera.position, 'z', -10, 10)
+cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'far', 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.open()
 
 function animate() {
   requestAnimationFrame(animate)
 
-  // stats.begin();
-  // cube.rotation.x += 0.01
-  // cube.rotation.y += 0.01
-  // stats.end();
+  camera.lookAt(0, 0.5, 0)
 
-  // renderer.render(scene, camera)
-  renderer.render(activeScene, camera)
-  stats.update();
+  renderer.render(scene, camera)
+
+  stats.update()
 }
 
 animate()
