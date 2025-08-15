2e716de6b6268cb253340d97992193a27a67289d
phopha <148693028+phopha@users.noreply.github.com>
2025-08-15 15:42:12 +0700

    12. Animation Loop

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement
-// const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true})
-const renderer = new THREE.WebGLRenderer({antialias: true})
-// renderer.setSize(200, 200)
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -18,9 +16,14 @@ window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.render(scene, camera);
 })
 
-new OrbitControls(camera, renderer.domElement)
+// new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', function(){
+  renderer.render(scene, camera)
+});
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
@@ -28,10 +31,23 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-function animate() {
-  requestAnimationFrame(animate)
+const stats = new Stats()
+document.body.appendChild(stats.dom)
 
-  renderer.render(scene, camera)
-}
+// const clock = new THREE.Clock()
+// let delta;
+// function animate() {
+//   requestAnimationFrame(animate)
 
-animate()
+//   delta = clock.getDelta();
+
+//   cube.rotation.x += 0.1 * delta;
+//   cube.rotation.y += 0.1 * delta;
+
+//   renderer.render(scene, camera)
+
+//   stats.update()
+// }
+
+// animate()
+renderer.render(scene, camera)
