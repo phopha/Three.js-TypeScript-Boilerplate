00da2ce0b57d40d4aca0e04b90331c2b58c6a4bc
phopha <148693028+phopha@users.noreply.github.com>
2025-08-15 16:08:47 +0700

    13. Object3D

--- src/main.ts
+++ src/main.ts
@@ -2,52 +2,68 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
+camera.position.set(1, 2, 3)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.render(scene, camera);
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-// new OrbitControls(camera, renderer.domElement)
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.addEventListener('change', function(){
-  renderer.render(scene, camera)
-});
+new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
+// cube.position.y = 1;
+// cube.rotation.x = Math.PI / 4;
+cube.scale.set(0.1, 2, 3);
 scene.add(cube)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-// const clock = new THREE.Clock()
-// let delta;
-// function animate() {
-//   requestAnimationFrame(animate)
+const gui = new GUI()
 
-//   delta = clock.getDelta();
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube, 'visible')
+cubeFolder.open()
 
-//   cube.rotation.x += 0.1 * delta;
-//   cube.rotation.y += 0.1 * delta;
+const positionFolder = cubeFolder.addFolder('Position')
+positionFolder.add(cube.position, 'x', -5, 5)
+positionFolder.add(cube.position, 'y', -5, 5)
+positionFolder.add(cube.position, 'z', -5, 5)
+positionFolder.open()
 
-//   renderer.render(scene, camera)
+const rotationFolder = cubeFolder.addFolder('Rotation')
+rotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+rotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+rotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+rotationFolder.open()
 
-//   stats.update()
-// }
+const scaleFolder = cubeFolder.addFolder('Scale')
+scaleFolder.add(cube.scale, 'x', -5, 5)
+scaleFolder.add(cube.scale, 'y', -5, 5)
+scaleFolder.add(cube.scale, 'z', -5, 5)
+scaleFolder.open()
 
-// animate()
-renderer.render(scene, camera)
+function animate() {
+    requestAnimationFrame(animate)
+
+    renderer.render(scene, camera)
+
+    stats.update()
+}
+
+animate()
