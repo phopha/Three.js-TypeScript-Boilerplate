2f2b8daf0d9d952f979e818f8f7146e7b7b17ce4
phopha <148693028+phopha@users.noreply.github.com>
2025-08-19 21:05:58 +0700

    24. GLTF Loader : Part 1

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,31 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import Stats from 'three/addons/libs/stats.module.js'
+import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js'
 
 const scene = new THREE.Scene()
 
-await new RGBELoader().loadAsync('img/venice_sunset_1k.hdr').then((texture) => {
+const light = new THREE.SpotLight(undefined, Math.PI * 1000)
+light.position.set(5, 5, 5)
+light.angle = Math.PI / 16
+light.castShadow = true
+scene.add(light)
+
+const helper = new THREE.SpotLightHelper(light)
+scene.add(helper)
+
+new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
   texture.mapping = THREE.EquirectangularReflectionMapping
   scene.environment = texture
-  scene.background = texture
-  scene.backgroundBlurriness = 1.0
 })
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(2, 1, -2)
+camera.position.set(1.5, 0.75, 2)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 0.1
+renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -29,46 +39,26 @@ window.addEventListener('resize', () => {
 })
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.y = 0.75
 controls.enableDamping = true
 
-// const loader = new GLTFLoader()
-// let suvBody: THREE.Object3D
-// await loader.loadAsync('models/suv_body.glb').then((gltf) => {
-//   suvBody = gltf.scene
-// })
+const textureLoader = new THREE.TextureLoader()
+const textureFlare0 = textureLoader.load('https://cdn.jsdelivr.net/gh/Sean-Bradley/First-Car-Shooter@main/dist/client/img/lensflare0.png')
 
-// await loader.loadAsync('models/suv_wheel.glb').then((gltf) => {
-//   const wheels = [gltf.scene, gltf.scene.clone(), gltf.scene.clone(), gltf.scene.clone()]
-//   wheels[0].position.set(-0.65, 0.2, -0.77)
-//   wheels[1].position.set(0.65, 0.2, -0.77)
-//   wheels[1].rotateY(Math.PI)
-//   wheels[2].position.set(-0.65, 0.2, 0.57)
-//   wheels[3].position.set(0.65, 0.2, 0.57)
-//   wheels[3].rotateY(Math.PI)
-//   suvBody.add(...wheels)
-//   scene.add(suvBody)
-// })\
+const lensflare = new Lensflare()
+lensflare.addElement(new LensflareElement(textureFlare0, 1000, 0))
+light.add(lensflare)
 
-async function loadCar() {
-  const loader = new GLTFLoader()
-  const [...model] = await Promise.all([loader.loadAsync('models/suv_body.glb'), loader.loadAsync('models/suv_wheel.glb')])
+new GLTFLoader().load('models/suzanne_scene.glb', (gltf) => {
+  console.log(gltf)
 
-  const suvBody = model[0].scene
-  const wheels = [model[1].scene, model[1].scene.clone(), model[1].scene.clone(), model[1].scene.clone()]
+  const suzanne = gltf.scene.getObjectByName('Suzanne') as THREE.Mesh
+  suzanne.castShadow = true
 
-  wheels[0].position.set(-0.65, 0.2, -0.77)
-  wheels[1].position.set(0.65, 0.2, -0.77)
-  wheels[1].rotateY(Math.PI)
-  wheels[2].position.set(-0.65, 0.2, 0.57)
-  wheels[3].position.set(0.65, 0.2, 0.57)
-  wheels[3].rotateY(Math.PI)
-  suvBody.add(...wheels)
+  const plane = gltf.scene.getObjectByName('Plane') as THREE.Mesh
+  plane.receiveShadow = true
 
-  scene.add(suvBody)
-}
-await loadCar()
-
+  scene.add(gltf.scene)
+})
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
