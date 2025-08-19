78f855a95d36fd3d18d781bcb55984852a18de74
phopha <148693028+phopha@users.noreply.github.com>
2025-08-20 00:18:36 +0700

    25. GLTF Loader : Part 2

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,17 @@ import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js'
 
 const scene = new THREE.Scene()
 
-const light = new THREE.SpotLight(undefined, Math.PI * 1000)
-light.position.set(5, 5, 5)
-light.angle = Math.PI / 16
-light.castShadow = true
-scene.add(light)
-
-const helper = new THREE.SpotLightHelper(light)
-scene.add(helper)
+// const light = new THREE.SpotLight(undefined, Math.PI * 1000)
+// light.position.set(5, 5, 5)
+// light.angle = Math.PI / 16
+// light.castShadow = true
+// scene.add(light)
 
 new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
   texture.mapping = THREE.EquirectangularReflectionMapping
   scene.environment = texture
+  scene.background = texture
+  scene.backgroundBlurriness = 0.15
 })
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
@@ -27,7 +26,7 @@ camera.position.set(1.5, 0.75, 2)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 0.1
+renderer.toneMappingExposure = 0.8
 renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
@@ -41,22 +40,33 @@ window.addEventListener('resize', () => {
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
-const textureLoader = new THREE.TextureLoader()
-const textureFlare0 = textureLoader.load('https://cdn.jsdelivr.net/gh/Sean-Bradley/First-Car-Shooter@main/dist/client/img/lensflare0.png')
 
-const lensflare = new Lensflare()
-lensflare.addElement(new LensflareElement(textureFlare0, 1000, 0))
-light.add(lensflare)
 
 new GLTFLoader().load('models/suzanne_scene.glb', (gltf) => {
-  console.log(gltf)
-
   const suzanne = gltf.scene.getObjectByName('Suzanne') as THREE.Mesh
-  suzanne.castShadow = true
+  suzanne.castShadow = true;
+
+  // console.log(suzanne.material);
+  // ((suzanne.material as THREE.MeshStandardMaterial).map as THREE.Texture).colorSpace = THREE.LinearSRGBColorSpace;
+
+  // @ts-ignore
+  suzanne.material.map.colorSpace = THREE.LinearSRGBColorSpace;
+  
 
   const plane = gltf.scene.getObjectByName('Plane') as THREE.Mesh
   plane.receiveShadow = true
 
+  const spotLight = gltf.scene.getObjectByName('Spot') as THREE.SpotLight
+  spotLight.intensity /= 500;
+  spotLight.castShadow = true;
+
+  const textureLoader = new THREE.TextureLoader()
+  const textureFlare0 = textureLoader.load('https://cdn.jsdelivr.net/gh/Sean-Bradley/First-Car-Shooter@main/dist/client/img/lensflare0.png')
+
+  const lensflare = new Lensflare()
+  lensflare.addElement(new LensflareElement(textureFlare0, 1000, 0))
+  spotLight.add(lensflare)
+ 
   scene.add(gltf.scene)
 })
 
